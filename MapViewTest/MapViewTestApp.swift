3c2776d0c68eb8dc7e160637855b4f5e000b3f1a
Nico <60318513+NicoMarksman@users.noreply.github.com>
2024-03-13 12:24:25 +0100

    initial

---
+++ MapViewTest/MapViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewTestApp.swift
+//  MapViewTest
+//
+//  Created by Nico on 13.03.24.
+//
+
+import SwiftUI
+
+@main
+struct MapViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
